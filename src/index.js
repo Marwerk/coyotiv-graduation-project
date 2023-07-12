2d7ff49077033f29ae95029bc74dbb996593b567
Marwerk <137423785+landlocked-farmer@users.noreply.github.com>
2023-07-12 15:44:54 +0000

    Creating my first User

--- src/index.js
+++ src/index.js
@@ -1,2 +1,14 @@
 const Users = require('./users')
 const PhotoGallery = require('./photo-gallery')
+
+const marvin = new Users(
+  'Marvin',
+  'Werkmesiter',
+  04917672274579,
+  'marvin.werkmeister@outlook.com',
+  'Kolonnenstr. 20, 10829',
+  'Berlin',
+  '10829'
+)
+
+console.log(marvin)
