47d6fd0cbba881520f14da9d93d0879c98634bf6
Marwerk <marvin.saccarello@outlook.com>
2023-07-14 15:44:18 +0200

    Changed class and file names to singular

---
+++ .history/src/models/booking_20230714153812.js
@@ -0,0 +1,14 @@
+class Booking {
+  constructor(id, userId, roomId, checkInDate, checkOutDate, numberOfGuests, totalPrice, bookingStatus) {
+    this.id = id
+    this.userId = userId;
+    this.roomId = roomId
+    this.checkInDate = checkInDate
+    this.checkOutDate = checkOutDate
+    this.numberOfGuests = numberOfGuests
+    this.totalPrice = totalPrice
+    this.bookingStatus = bookingStatus
+  }
+}
+
+module.exports = Booking
