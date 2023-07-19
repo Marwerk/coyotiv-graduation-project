74afb4698580a313eab0ad6d07a82f3268710a70
Marvin Saccarello Werkmeister <marvin.saccarello@outlook.com>
2023-07-19 19:24:37 +0000

    Create second instance of Booking

--- src/model/booking.js
+++ src/model/booking.js
@@ -16,6 +16,7 @@ class Booking {
 // How do I add the guestId to the booking without manually inputting it as a parameter?
 // As in, when the user creates a booking, how is the guestId added to the booking?
 const booking1 = new Booking(1, 1, 1, '2021-01-01', '2021-01-03', 2, 200, 'confirmed')
+const booking2 = new Booking(2, 1, 2, '2021-01-01', '2021-01-03', 1, 100, 'confirmed')
 
 // Tests
 // console.log(`These are the entries of ${booking1}:`)
