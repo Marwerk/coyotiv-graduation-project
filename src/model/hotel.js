21508a0109890e02fa51d167853b36943fdc2709
Marvin Saccarello Werkmeister <marvin.saccarello@outlook.com>
2023-07-19 12:51:38 +0000

    Add addRoom method to Class Hotel

--- src/model/hotel.js
+++ src/model/hotel.js
@@ -1,11 +1,16 @@
 class Hotel {
-  constructor(name, location, description, rating, amenities, imageUrl) {
+  constructor(name, location, description, rating, amenities, imageUrl, rooms) {
     this.name = name
     this.location = location
     this.description = description
     this.rating = rating
     this.amenities = amenities
     this.imageUrl = imageUrl
+    this.rooms = []
+  }
+
+  addRoom(room) {
+    this.rooms.push(room)
   }
 }
 
