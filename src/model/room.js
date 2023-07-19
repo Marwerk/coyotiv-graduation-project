1cda3ead8707f933e6209884fe4ebb4990c6ee88
Marvin Saccarello Werkmeister <marvin.saccarello@outlook.com>
2023-07-19 12:52:04 +0000

    Add Rooms to new Hotel instance

--- src/model/room.js
+++ src/model/room.js
@@ -1,7 +1,10 @@
 const chalk = require('chalk')
+const Hotel = require('./hotel')
+
+const lasCalas = new Hotel('Las Calas Apart-Hotel', 'Francisco Zurbaran 1560, Asuncion, Paraguay')
 
 class Room {
-  constructor(roomId, name, description, roomType, maxOccupancy, pricePerNight, amenities) {
+  constructor(roomId, name, description, maxOccupancy, pricePerNight, amenities) {
     this.roomId = roomId
     this.name = name
     this.description = description
@@ -11,11 +14,14 @@ class Room {
     this.amenities = amenities
   }
 
+  // newDetails is an object with the new values for the room
   updateRoomDetails(newDetails) {
     const keys = ['name', 'description', 'maxOccupancy', 'pricePerNight', 'amenities']
     keys.forEach(key => {
       this[key] = newDetails[key] || this[key]
     })
+    // example of what this method does:
+    // myRoom.updateRoomDetails({ name: 'Updated Room Name', maxOccupancy: 3 })
   }
 }
 
@@ -31,6 +37,8 @@ const doubleRoom = new Room(1, 'Double Room', 'Two Single Size Beds', 2, 100, [
   'Breakfast Included',
 ])
 
+lasCalas.addRoom(doubleRoom)
+
 // There is a total of 3 queenRoom in the Hotel, one has an external bathroom (roomId = 6)
 const queenRoom = new Room(2, 'Single Room', 'One Queen Size Bed', 1, 100, [
   'Private Bathroom',
@@ -41,14 +49,18 @@ const queenRoom = new Room(2, 'Single Room', 'One Queen Size Bed', 1, 100, [
   'Breakfast Included',
 ])
 
+lasCalas.addRoom(queenRoom)
+
 const queenRoom6 = new Room(6, 'Single Room', 'One Queen Size Bed', 1, 100, [
-  'Private External Bathroom',
+  'Private External Bathroom', // perhaps "adjacent" is a better word than "external?
   'Wifi',
   'TV',
   'Air Conditioning',
   'Garden View',
   'Breakfast Included',
 ])
+
+lasCalas.addRoom(queenRoom6)
 
 // There is a total of 3 kingRoom in the Hotel
 const kingRoom = new Room(3, 'Suite Room', 'One King Size Bed', 2, 200, [
@@ -60,6 +72,10 @@ const kingRoom = new Room(3, 'Suite Room', 'One King Size Bed', 2, 200, [
   'Breakfast Included',
 ])
 
+lasCalas.addRoom(kingRoom)
+
+// Tests
+console.log(lasCalas.rooms)
 console.log(`queenRoom6 should have a roomId of 6: ${queenRoom6.roomId === 6 ? chalk.green('✓') : chalk.red('✗')}`)
 
 module.exports = Room
