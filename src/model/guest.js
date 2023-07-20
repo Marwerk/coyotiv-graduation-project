546ea9390257b55cd66926ff935f8cad2195ed52
Marvin Saccarello Werkmeister <marvin.saccarello@outlook.com>
2023-07-20 09:16:27 +0000

    Polish Guest class based on Armagan's feedback

--- src/model/guest.js
+++ src/model/guest.js
@@ -1,13 +1,11 @@
-const booking1 = require('./booking')
+const Booking = require('./booking')
 
 class Guest {
-  constructor(guestId, firstName, lastName, phoneNumber, email, password, address, city, paymentMethod, creationDate) {
-    this.guestId = guestId
+  constructor(firstName, lastName, phoneNumber, email, address, city, paymentMethod, creationDate) {
     this.firstName = firstName
     this.lastName = lastName
     this.phoneNumber = phoneNumber
     this.email = email
-    this.password = password
     this.address = address
     this.city = city
     this.paymentMethod = paymentMethod
@@ -17,58 +15,11 @@ class Guest {
 
   // "Setter" method to add booking.
   // I don't use an actual setter method because I don't want to overwrite the array of bookings.
-  addBooking(bookingId) {
+  book(bookingId) {
     this.bookings.push(bookingId)
     // .push() to add new booking to array of bookings.
   }
-
-  // Getter method to get all bookings.
-  get allBookings() {
-    return this.bookings
-  }
-
-  // Method to update guest details. Based on Taiga's solution.
-  updateGuestDetails(newDetails) {
-    const keys = ['firstName', 'lastName', 'phoneNumber', 'email', 'password', 'address', 'city', 'paymentMethod']
-    keys.forEach(key => {
-      this[key] = newDetails[key] || this[key]
-      // forEach loop to iterate over each key.
-      // For each key, we update the value if newDetails has a new value for that key; otherwise, we keep the current value.
-    })
-  }
-  // First attempt at updateUserDetails method.
-  // This method works, but it's not very DRY. (Did I just apply premature optimization?!)
-  //
-  // updateUserDetails(newDetails) {
-  //   this.firstName = newDetails.firstName || this.firstName
-  //   this.lastName = newDetails.lastName || this.lastName
-  //   this.phoneNumber = newDetails.phoneNumber || this.phoneNumber
-  //   this.email = newDetails.email || this.email
-  //   this.password = newDetails.password || this.password
-  //   this.address = newDetails.address || this.address
-  //   this.city = newDetails.city || this.city
-  //   this.paymentMethod = newDetails.paymentMethod || this.paymentMethod
-  // }
 }
-
-const guest1 = new Guest(
-  1,
-  'Marvin',
-  'Werkmeister',
-  '017672274578',
-  'marvin.blerg@gmail.com',
-  'password123',
-  'Timbuktustraße 3',
-  'Berlin',
-  'PayPal',
-  '2023-07-19'
-)
-guest1.addBooking(booking1)
-// console.log(guest1.allBookings)
-
-// https://github.com/coyotiv-sose/graduation-project-marwerk/issues/8
-// Why is this not working? I was able to add booking1 without issues
-// guest1.addBooking(booking2)
 
 // ------------------------------------------------------------------------
 module.exports = Guest
