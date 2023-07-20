2120add9b983e5e9f3776a1d9cde1c3be0dc4263
Marvin Saccarello Werkmeister <marvin.saccarello@outlook.com>
2023-07-20 09:09:43 +0000

    Move Guest instance to index.js

--- src/index.js
+++ src/index.js
@@ -3,3 +3,15 @@ const Guest = require('./model/guest')
 const Room = require('./model/room')
 const Hotel = require('./model/hotel')
 const Booking = require('./model/booking')
+
+const guest1 = new Guest(
+  1,
+  'Marvin',
+  'Werkmeister',
+  '017672274578',
+  'marvin.blerg@gmail.com',
+  'Timbuktustraße 3',
+  'Berlin',
+  'PayPal',
+  '2023-07-19'
+)
