d67768360f9eaf9c00c92df0f0c262eaba003ffa
Marvin Saccarello Werkmeister <marvin.saccarello@outlook.com>
2023-07-20 09:14:08 +0000

    Polish booking class based on Armagan's feedback

--- src/model/booking.js
+++ src/model/booking.js
@@ -1,23 +1,18 @@
 const chalk = require('chalk')
 
 class Booking {
-  constructor(bookingId, guestId, roomId, checkInDate, checkOutDate, numberOfGuests, totalPrice, bookingStatus) {
-    this.bookingId = bookingId
+  constructor(bookingCode, guestId, roomId, checkInDate, checkOutDate, numberOfGuests, totalPrice, bookingStatus) {
+    this.bookingCode = bookingCode
     this.guestId = guestId
     this.roomId = roomId
     this.checkInDate = checkInDate
     this.checkOutDate = checkOutDate
     this.numberOfGuests = numberOfGuests
-    this.totalPrice = totalPrice
     this.bookingStatus = bookingStatus
   }
 }
 
-// https://github.com/coyotiv-sose/graduation-project-marwerk/issues/7
-// How do I add the guestId to the booking without manually inputting it as a parameter?
-// As in, when the user creates a booking, how is the guestId added to the booking?
-const booking1 = new Booking(1, 1, 1, '2021-01-01', '2021-01-03', 2, 200, 'confirmed')
-const booking2 = new Booking(2, 1, 2, '2021-01-01', '2021-01-03', 1, 100, 'confirmed')
+// create a getter method for totalPrice
 
 // ------------------------------------------------------------------------
 module.exports = Booking
