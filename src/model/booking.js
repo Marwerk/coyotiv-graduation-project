6013463b348d1a71814ee60133ff2ae9799ee2d7
Marvin Saccarello Werkmeister <marvin.saccarello@outlook.com>
2023-07-21 09:50:48 +0000

    Simplify Booking Class key:value pairs

--- src/model/booking.js
+++ src/model/booking.js
@@ -1,16 +1,8 @@
-const chalk = require('chalk')
-
 class Booking {
-  constructor(guest, checkInDate, checkOutDate, numberOfGuests) {
+  constructor(guest) {
     this.guest = guest
-    this.checkInDate = checkInDate
-    this.checkOutDate = checkOutDate
-    this.numberOfGuests = numberOfGuests
-    // this.bookingStatus = bookingStatus // for a future implementation
   }
 }
-
-// create a getter method for totalPrice
 
 // ------------------------------------------------------------------------
 module.exports = Booking
