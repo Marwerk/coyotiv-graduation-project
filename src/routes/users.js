3c1d74ecfbf96ffa59a2306b20f6da16d4056846
Marvin Saccarello Werkmeister <marvin.saccarello@outlook.com>
2023-07-24 18:22:08 +0000

    Test PUG rendering

--- src/routes/users.js
+++ src/routes/users.js
@@ -4,7 +4,12 @@ const router = express.Router()
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-  res.send([{ name: 'John' }, { name: 'Doe' }])
+  res.render('users', {
+    user: {
+      name: 'John Doe',
+    },
+    users: [{ name: 'John Doe' }, { name: 'Jane Doe' }, { name: 'Jim Doe' }],
+  })
 })
 
 module.exports = router
