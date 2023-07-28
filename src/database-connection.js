39f4b71f11e07ae7aaaca6828db9373f5865ee32
Marvin Saccarello Werkmeister <marvin.saccarello@outlook.com>
2023-07-28 23:01:22 +0000

    Take flight

--- src/database-connection.js
+++ src/database-connection.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose')
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING)
-const Cat = mongoose.model('Cat', { name: String })
+const Cat = mongoose.model('Cat', { name: String, free: Boolean })
 
-const kitty = new Cat({ name: 'Horus' })
-kitty.save().then(() => console.log('meow'))
+try {
+  const kitty = new Cat({ name: 'Horus', free: true })
+  kitty.save().then(() => console.log('meow'))
+} catch (error) {
+  console.log('This is the error:', error)
+}
