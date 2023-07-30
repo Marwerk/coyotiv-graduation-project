c4515d44aa25d3d9256111841adf6de81867abd9
Marvin Saccarello Werkmeister <marvin.saccarello@outlook.com>
2023-07-30 11:05:25 +0000

    Introduce bookingSchema

--- src/models/booking.js
+++ src/models/booking.js
@@ -1,5 +1,12 @@
+const mongoose = require('mongoose')
+
 const MILLISECONDS_IN_A_DAY = 1000 * 60 * 60 * 24
 
+const bookingSchema = new mongoose.Schema({
+  guest: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  checkInDate: Date,
+  checkOutDate: Date,
+})
 class Booking {
   constructor(guest, checkInDate, checkOutDate) {
     this.guest = guest
@@ -27,4 +34,5 @@ class Booking {
   static list = []
 }
 // ------------------------------------------------------------------------
-module.exports = Booking
+// module.exports = Booking
+module.exports = mongoose.model('Booking', bookingSchema)
