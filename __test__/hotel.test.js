daa6eacbf03be3dfb9d2af3521c44933eb61c0ff
Marvin Saccarello Werkmeister <marvin.saccarello@outlook.com>
2023-08-07 09:18:12 +0000

    Polish hotel.test.js

--- __test__/hotel.test.js
+++ __test__/hotel.test.js
@@ -2,7 +2,7 @@ const Hotel = require('../src/models/hotel')
 
 it('should add a room to the hotel', async () => {
   // Mock the Hotel instance and its save method
-  const mockSave = jest.fn().mockResolvedValue(true) // Mock it to resolve true for simplicity
+  const mockSave = jest.fn().mockResolvedValue(true)
   const mockHotel = {
     rooms: [],
     save: mockSave,
@@ -10,7 +10,7 @@ it('should add a room to the hotel', async () => {
 
   const room = { _id: 'room1' }
 
-  // Call the addRoom prototype method directly using .call()
+  // .call() the .addRoom method of the Hotel instance
   await Hotel.prototype.addRoom.call(mockHotel, room)
 
   expect(mockHotel.rooms).toContain(room)
