df3ee729251d1241096b064c011c9da0c71ccf8c
Marvin Saccarello Werkmeister <marvin.saccarello@outlook.com>
2023-08-07 07:44:13 +0000

    Typos

--- __test__/room.test.js
+++ __test__/room.test.js
@@ -1,26 +1,16 @@
 const Room = require('../src/models/room')
 
-// Reset all mocks before each
-beforeEach(() => {
-  jest.resetAllMocks()
-})
-
 // Test Case 1: Checking availability when room is available
+it('should return true if room savailable', async () => {
+  const room = new Room()
+  room.units = 2
+  room.bookings = [
+    {
+      checkInDate: new Date('2023-08-10'),
+      checkOutDate: new Date('2023-08-15'),
+    },
+  ]
 
-describe('Room', () => {
-  describe('checkAvailability', () => {
-    it('should return true if room savailable', async () => {
-      const room = new Room()
-      room.units = 2
-      room.bookings = [
-        {
-          checkInDate: new Date('2023-08-10'),
-          checkOutDate: new Date('2023-08-15'),
-        },
-      ]
-
-      const result = await room.checkAvailability(new Date('2023-08-05'), new Date('2023-08-09'))
-      expect(result).toBe(true)
-    })
-  })
+  const result = await room.checkAvailability(new Date('2023-08-05'), new Date('2023-08-09'))
+  expect(result).toBe(true)
 })
