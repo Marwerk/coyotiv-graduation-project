3718dcbdde84abd0576324fef09244b88c7e62ff
Marvin Saccarello Werkmeister <marvin.saccarello@outlook.com>
2023-08-07 22:49:58 +0000

    Revert previous commit

---
+++ backend/test/room.test.js
@@ -0,0 +1,16 @@
+const Room = require('../../src/models/room')
+
+// Test Case 1: Checking availability when room is available
+it('should return true if room savailable', async () => {
+  const room = new Room()
+  room.units = 2
+  room.bookings = [
+    {
+      checkInDate: new Date('2023-08-10'),
+      checkOutDate: new Date('2023-08-15'),
+    },
+  ]
+
+  const result = await room.checkAvailability(new Date('2023-08-05'), new Date('2023-08-09'))
+  expect(result).toBe(true)
+})
