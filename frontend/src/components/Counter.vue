a669f76d2643b767e144b1afad7d7f020ccfbed3
Marvin Saccarello Werkmeister <marvin.saccarello@outlook.com>
2023-08-08 23:23:42 +0000

    Show difference between imperative and declarative with counter component

---
+++ frontend/src/components/Counter.vue
@@ -0,0 +1,23 @@
+<!-- Imperative Programming -->
+
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+
+const increment = () => {
+  count.value++
+}
+
+const decrement = () => {
+  count.value--
+}
+</script>
+
+<template lang="pug">
+.counter
+  h1 Counter
+  p The count is: {{ count }}
+  button(@click='increment') Increment
+  button(@click='decrement') Decrement
+</template>
