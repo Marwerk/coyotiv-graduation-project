5199dd3b8ff83477a908a997d600cea821f92a89
Marvin Saccarello Werkmeister <marvin.saccarello@outlook.com>
2023-08-08 16:56:38 +0000

    Create new View part 2

---
+++ frontend/src/views/UsersView.vue
@@ -0,0 +1,18 @@
+<script setup>
+import axios from 'axios'
+
+const usersResponse = await axios.get('http://localhost:3000/users')
+
+const users = usersResponse.data
+</script>
+
+<template>
+  <div class="users">
+    <h1>Users</h1>
+    <ul>
+      <li v-for="user in users" :key="user.id">
+        {{ user.firstName }}
+      </li>
+    </ul>
+  </div>
+</template>
