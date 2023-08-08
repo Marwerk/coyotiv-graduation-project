2df94541bb9ec224d5fd509d2558b0207b51ea10
Marvin Saccarello Werkmeister <marvin.saccarello@outlook.com>
2023-08-08 16:56:06 +0000

    Create new View

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import UsersView from '../views/UsersView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,8 +11,14 @@ const router = createRouter({
       component: HomeView
     },
     {
+      path: '/users',
+      name: 'users',
+      component: UsersView
+    },
+    {
       path: '/about',
       name: 'about',
+      // TODO: vue components homework
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
