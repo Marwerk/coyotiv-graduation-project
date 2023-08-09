a17fdf76d42a863c7bba43537fa99b71ad1dbdc0
Marvin Saccarello Werkmeister <marvin.saccarello@outlook.com>
2023-08-09 22:49:40 +0000

    Attempt to use Pinia

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,8 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { useRoomStore } from './stores/RoomStore'
+
+useRoomStore()
 </script>
 
 <template>
