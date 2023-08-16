13ce8402aeb41bf4beac250f2ff625a4db2a2a14
Marvin Saccarello Werkmeister <marvin.saccarello@outlook.com>
2023-08-16 22:51:49 +0000

    WIP issue with base url

--- backend/src/routes/accounts.js
+++ backend/src/routes/accounts.js
@@ -1,39 +1,32 @@
+/* eslint-disable consistent-return */
 const express = require('express')
 
 const router = express.Router()
 const passport = require('passport')
+
 const User = require('../models/user')
 
-router.get('/session', async function (req, res, next) {
-  if (!req.user) return next({ status: 401, message: 'Not logged in!' })
-  console.log('the current user is', req.user)
+router.get('/session', async (req, res, next) => {
+  if (!req.user) return next({ status: 401, message: 'Unauthorized' })
+
   res.send(req.user)
 })
 
+// Login
 router.post(
   '/session',
   passport.authenticate('local', { failWithError: true }),
-  function (req, res) {
+  (req, res, next) => {
     res.send(req.user)
   }
 )
 
-router.delete('/session', function (req, res) {
+// Logout
+router.delete('/session', (req, res, next) => {
   req.logout(() => {
     res.sendStatus(200)
   })
 })
-// router.delete('/session', (req, res) => {
-//   // return to appease the eslint overlords
-//   return req.logout(err => {
-//     if (err) return res.sendStatus(500)
-
-//     // return to appease the eslint overlords
-//     return req.session.destroy(error => {
-//       return error ? res.sendStatus(500) : res.sendStatus(200)
-//     })
-//   })
-// })
 
 // ------------------------------------------------------------------------ //
 
--- frontend/src/stores/AccountStore.js
+++ frontend/src/stores/AccountStore.js
@@ -2,27 +2,19 @@ import { defineStore } from 'pinia'
 import axios from 'axios'
 
 axios.defaults.withCredentials = true
-axios.defaults.baseURL = 'http://localhost:3000/'
+axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL
 
-export const useAccountStore = defineStore('Account', {
+export const useAccountStore = defineStore('account', {
   state: () => ({
     user: null
   }),
   actions: {
     async fetchUser() {
-      try {
-        this.user = (await axios.get('/accounts/session')).data
-      } catch (error) {
-        console.log('Error in line 16 of AccountStore.js', error)
-      }
+      this.user = (await axios.get('/accounts/session')).data
     },
     async login(email, password) {
-      this.user = (
-        await axios.post('/accounts/session', {
-          email: email,
-          password: password
-        })
-      ).data
+      const response = await axios.post('/accounts/session', { email, password })
+      this.user = response.data
     },
     async logout() {
       await axios.delete('/accounts/session')
