dac46396f3d48bba628f0efee47a281d824d17f2
Marvin Saccarello Werkmeister <marvin.saccarello@outlook.com>
2023-08-17 08:05:23 +0000

    Remove error.pug render

--- backend/src/app.js
+++ backend/src/app.js
@@ -93,13 +93,13 @@ app.use(function (req, res, next) {
   next(createError(404))
 })
 
-// Express.js error handler. Renders error.pug page
+// Express.js error handler
 app.use(function (err, req, res, next) {
   res.locals.message = err.message
   res.locals.error = req.app.get('env') === 'development' ? err : {}
 
   res.status(err.status || 500)
-  res.render('error')
+  res.send(err)
 })
 
 module.exports = app
