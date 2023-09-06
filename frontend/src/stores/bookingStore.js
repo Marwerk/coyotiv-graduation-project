feee80957bc4c4c8cb993f69379a79433933e790
Marvin Saccarello Werkmeister <marvin.saccarello@outlook.com>
2023-09-06 16:47:16 +0000

    Fetch bookings based on user Role (all for admin, user-created for user)

--- frontend/src/stores/bookingStore.js
+++ frontend/src/stores/bookingStore.js
@@ -5,9 +5,12 @@ axios.defaults.withCredentials = true
 axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL
 
 export const useBookingStore = defineStore('Booking', {
+  state: () => ({
+    allBookings: []
+  }),
   actions: {
     async fetchBookings() {
-      return (await axios.get('/bookings')).data
+      this.allBookings = (await axios.get('/bookings')).data
     },
 
     async bookRoom(userId, type, checkIn, checkOut) {
